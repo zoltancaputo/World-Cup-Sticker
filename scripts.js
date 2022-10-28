277b86559ea64ceb24e29709f96d65fd056a0033
Zoltanbh <zoltancaputo@gmail.com.br>
2022-10-28 17:48:44 -0300

    commit 4

--- scripts.js
+++ scripts.js
@@ -6,3 +6,24 @@ function changeCard(event) {
   isIgnite = !isIgnite
   card.style.backgroundImage = `url(./assets/bg-${bg}.svg)`
 }
+
+// joking whith switch case bellow
+
+let corfundo;
+corfundo = prompt("Olá, Digite nesta caixa uma cor e tente acertar a cor de fundo da página!");
+
+switch(corfundo) {
+  case "verde":  
+    alert("Essa cor é do IGNITE!"); 
+    break;
+  case"roxo" || "roxo": 
+    alert("Essar cor é do EXPLORER!");
+   break;
+  case "branco":
+    alert("Essa cor iremos implementar!");
+    break;
+  default: // padrão - ausencia de dados
+    alert("Cor errada!");
+    break;
+
+}
